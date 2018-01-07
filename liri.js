b4324e254a5dd25f44132c6f6fe656a85265b635
Abby Thoresen <akthoresen39@gmail.com>
2018-01-07 13:24:34 -0700

    inital file structures are set up

---
+++ liri.js
@@ -0,0 +1,19 @@
+// write the code i need. to grab data from keys.js
+// then store keys in a variable
+
+var Twitter = require('twitter');
+var Spotify = require('node-spotify-api');
+var request = require('request');
+var keys = require('./keys.js');
+
+console.log(keys.consumer_key)
+
+
+var userInput = process.argv[2]
+console.log(userInput)
+if (userInput === 'spotify-this-song'){
+  console.log('spoify-logic')
+
+var songTitle - process.argv[3]
+
+}

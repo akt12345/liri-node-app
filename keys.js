0c6ce9bb571fee6857da7fac09079236df998651
Abby Thoresen <akthoresen39@gmail.com>
2018-01-14 14:46:11 -0700

    set up variables and test thru console.log

--- keys.js
+++ keys.js
@@ -1,11 +1,23 @@
 console.log('this is loaded');
 
 var twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: '6pqH5Nn4ewFGMri5imjUYyBBC',
+  consumer_secret: 'G3TuPWc9bRpclO3B4GpdDOOHcfuycyQ1fpNrCi7whs2uH04SQn',
+  access_token_key: '951578736712761346-HaNvjeNuEznQAs7ac9ZLvzoMuhviWQr',
+  access_token_secret: 'uV3HvXS7LovpX1ciRPDNAj5yZtU7h97HCt3UQ48ELMwuS',
 }
 
-module.exports = twitterKeys;
+
+var spotifyKeys = {
+  id: '41d9bc0a6d6c40ed8c82a29acac04e10',
+  secret: 'G3TuPWc9bRpclO3B4GpdDOOHcfuycyQ1fpNrCi7whs2uH04SQn',
+}
+
+
+
+// module to export the oject (twitter & Spotify)
+module.exports = {
+  twitterKeys: twitterKeys,
+  spotifyKeys: spotifyKeys
+}
 
--- liri.js
+++ liri.js
@@ -1,19 +1,42 @@
 // write the code i need. to grab data from keys.js
 // then store keys in a variable
-
-var Twitter = require('twitter');
-var Spotify = require('node-spotify-api');
 var request = require('request');
 var keys = require('./keys.js');
+var Twitter = require('twitter');
+var twitterKeys = keys.twitterKeys;
+var Spotify = require('node-spotify-api');
+var spotifyKeys = keys.spotifyKeys;
+//console.log(spotifyKeys)
+var spotify = new Spotify(spotifyKeys);
+var fs = require('fs');
+var client = new Twitter(twitterKeys);
+var command = process.argv[2];
 
-console.log(keys.consumer_key)
+
+
+
 
 
 var userInput = process.argv[2]
-console.log(userInput)
+
+
 if (userInput === 'spotify-this-song'){
   console.log('spoify-logic')
 
-var songTitle - process.argv[3]
+var songTitle = process.argv[3]
+}
+else if (userInput === 'my-tweets'){
 
+var params = {screen_name: 'fancyPants_2016'};
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
+    /*console.log(tweets[0].text)*/
+  for(var i = 0; i < tweets.length; i ++ ){
+  console.log(tweets[i].text)
+  }
+
+
+
+  }
+});
 }
